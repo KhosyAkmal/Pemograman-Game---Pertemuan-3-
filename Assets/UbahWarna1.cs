593459e37f39f8ee0a975de12e77325160630160
KhosyAkmal <akmalkhosy@gmail.com>
2020-09-16 11:45:17 +0800

    init commit

---
+++ Assets/UbahWarna1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UbahWarna1 : MonoBehaviour
+{
+    public void Hijau()
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    public void Magenta()
+    {
+        GetComponent<Renderer>().material.color = Color.magenta;
+    }
+
+    public void Grey()
+    {
+        GetComponent<Renderer>().material.color = Color.grey;
+    }
+}
